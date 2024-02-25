f5e04692db1e1cc4408d773ee176118b9603f156
Kryštof Rainisch <krystof.rainisch@hey.com>
2024-02-25 14:43:14 +0100

    Add support for surfaces

--- Sources/RoomObjectReplicator/RoomObjectReplicator.swift
+++ Sources/RoomObjectReplicator/RoomObjectReplicator.swift
@@ -8,29 +8,36 @@
 import ARKit
 import RoomPlan
 
-public class RoomObjectAnchor: ARAnchor {
+public enum RoomElementCategory {
+    case object(CapturedRoom.Object.Category)
+    case surface(CapturedRoom.Surface.Category)
+}
 
-    public override var identifier: UUID {
-        roomObjectIdentifier
+public class RoomElementAnchor: ARAnchor {
+    override public var identifier: UUID {
+        roomElementIdentifier
     }
 
-    public override var transform: simd_float4x4 {
-        roomObjectTransform
+    override public var transform: simd_float4x4 {
+        roomElementTransform
     }
 
     public private(set) var dimensions: simd_float3
-    public private(set) var category: CapturedRoom.Object.Category
+    //    public private(set) var category: CapturedRoom.Object.Category
+    public private(set) var category: RoomElementCategory
 
-    private let roomObjectIdentifier: UUID
-    private var roomObjectTransform: simd_float4x4
+    private let roomElementIdentifier: UUID
+    private var roomElementTransform: simd_float4x4
 
     public required init(anchor: ARAnchor) {
-        guard let anchor = anchor as? RoomObjectAnchor else {
-            fatalError("RoomObjectAnchor can only copy other RoomObjectAnchor instances")
+        guard let anchor = anchor as? RoomElementAnchor else {
+            fatalError(
+                "RoomObjectAnchor can only copy other RoomObjectAnchor instances"
+            )
         }
 
-        roomObjectIdentifier = anchor.roomObjectIdentifier
-        roomObjectTransform = anchor.roomObjectTransform
+        roomElementIdentifier = anchor.roomElementIdentifier
+        roomElementTransform = anchor.roomElementTransform
         dimensions = anchor.dimensions
         category = anchor.category
 
@@ -38,56 +45,97 @@ public class RoomObjectAnchor: ARAnchor {
     }
 
     @available(*, unavailable)
-    public required init?(coder: NSCoder) {
+    public required init?(coder _: NSCoder) {
         fatalError("Unavailable")
     }
 
     public init(_ object: CapturedRoom.Object) {
-        roomObjectIdentifier = object.identifier
-        roomObjectTransform = object.transform
+        roomElementIdentifier = object.identifier
+        roomElementTransform = object.transform
         dimensions = object.dimensions
-        category = object.category
+        category = .object(object.category)
         super.init(transform: object.transform)
     }
 
     fileprivate func update(_ object: CapturedRoom.Object) {
-        roomObjectTransform = object.transform
+        roomElementTransform = object.transform
         dimensions = object.dimensions
-        category = object.category
+        category = .object(object.category)
     }
 
+    public init(_ surface: CapturedRoom.Surface) {
+        roomElementIdentifier = surface.identifier
+        roomElementTransform = surface.transform
+        dimensions = surface.dimensions
+        category = .surface(surface.category)
+        super.init(transform: surface.transform)
+    }
+
+    fileprivate func update(_ surface: CapturedRoom.Surface) {
+        roomElementTransform = surface.transform
+        dimensions = surface.dimensions
+        category = .surface(surface.category)
+    }
 }
 
 public class RoomObjectReplicator {
-
-    private var trackedAnchors: Set<RoomObjectAnchor>
-    private var trackedAnchorsByIdentifier: [UUID: RoomObjectAnchor]
-    private var inflightAnchors: Set<RoomObjectAnchor>
+    private var trackedAnchors: Set<RoomElementAnchor>
+    private var trackedAnchorsByIdentifier: [UUID: RoomElementAnchor]
+    private var inflightAnchors: Set<RoomElementAnchor>
 
     public init() {
-        trackedAnchors = Set<RoomObjectAnchor>()
-        trackedAnchorsByIdentifier = [UUID: RoomObjectAnchor]()
-        inflightAnchors = Set<RoomObjectAnchor>()
+        trackedAnchors = Set<RoomElementAnchor>()
+        trackedAnchorsByIdentifier = [UUID: RoomElementAnchor]()
+        inflightAnchors = Set<RoomElementAnchor>()
     }
 
-    public func anchor(objects: [CapturedRoom.Object], in session: RoomCaptureSession) {
+    public func anchor(
+        objects: [CapturedRoom.Object],
+        surfaces: [CapturedRoom.Surface],
+        in session: RoomCaptureSession
+    ) {
         for object in objects {
-            if let existingAnchor = trackedAnchorsByIdentifier[object.identifier] {
+            if
+                let existingAnchor =
+                trackedAnchorsByIdentifier[object.identifier]
+            {
                 existingAnchor.update(object)
                 inflightAnchors.insert(existingAnchor)
-                session.arSession.delegate?.session?(session.arSession, didUpdate: [existingAnchor])
-            } else {
-                let anchor = RoomObjectAnchor(object)
+                session.arSession.delegate?.session?(
+                    session.arSession,
+                    didUpdate: [existingAnchor]
+                )
+            }
+            else {
+                let anchor = RoomElementAnchor(object)
                 inflightAnchors.insert(anchor)
                 session.arSession.add(anchor: anchor)
             }
         }
-
+        for surface in surfaces {
+            if
+                let existingAnchor =
+                trackedAnchorsByIdentifier[surface.identifier]
+            {
+                existingAnchor.update(surface)
+                inflightAnchors.insert(existingAnchor)
+                session.arSession.delegate?.session?(
+                    session.arSession,
+                    didUpdate: [existingAnchor]
+                )
+            }
+            else {
+                let anchor = RoomElementAnchor(surface)
+                inflightAnchors.insert(anchor)
+                session.arSession.add(anchor: anchor)
+            }
+        }
         trackInflightAnchors(in: session)
     }
 
     private func trackInflightAnchors(in session: RoomCaptureSession) {
-        trackedAnchors.subtracting(inflightAnchors).forEach(session.arSession.remove)
+        trackedAnchors.subtracting(inflightAnchors)
+            .forEach(session.arSession.remove)
         trackedAnchors.removeAll(keepingCapacity: true)
         trackedAnchors.formUnion(inflightAnchors)
         inflightAnchors.removeAll(keepingCapacity: true)
@@ -97,5 +145,4 @@ public class RoomObjectReplicator {
             trackedAnchorsByIdentifier[trackedAnchor.identifier] = trackedAnchor
         }
     }
-
 }
--- Sources/RoomObjectReplicator/RoomObjectSystem.swift
+++ Sources/RoomObjectReplicator/RoomObjectSystem.swift
@@ -10,87 +10,128 @@ import RealityFoundation
 import RoomPlan
 
 public struct RoomObjectComponent: Component {
-
     public var dimensions: simd_float3 = .zero
     public var category: CapturedRoom.Object.Category? = nil
-
+}
+public struct RoomSurfaceComponent: Component {
+    public var dimensions: simd_float3 = .zero
+    public var category: CapturedRoom.Surface.Category? = nil
 }
 
 public protocol HasRoomObjectComponent {
-
     var roomObject: RoomObjectComponent? { get set }
-
 }
 
-public class RoomObjectEntity: Entity, HasAnchoring, HasModel, HasRoomObjectComponent {
+public protocol HasRoomSurfaceComponent {
+    var roomSurface: RoomSurfaceComponent? { get set }
+}
 
+
+public class RoomObjectEntity: Entity, HasAnchoring, HasModel, HasRoomObjectComponent, HasRoomSurfaceComponent {
     public var anchoring: AnchoringComponent? {
         get { components[AnchoringComponent.self] }
         set { components[AnchoringComponent.self] = newValue }
     }
-
+    
     public var model: ModelComponent? {
         get { components[ModelComponent.self] }
         set { components[ModelComponent.self] = newValue }
     }
-
+    
     public var roomObject: RoomObjectComponent? {
         get { components[RoomObjectComponent.self] }
         set { components[RoomObjectComponent.self] = newValue }
     }
-
+    public var roomSurface: RoomSurfaceComponent? {
+        get { components[RoomSurfaceComponent.self] }
+        set { components[RoomSurfaceComponent.self] = newValue }
+    }
+    
     public required convenience init() {
         self.init(dimensions: .zero)
     }
-
-    public convenience init(_ anchor: RoomObjectAnchor) {
+    
+    public convenience init(_ anchor: RoomElementAnchor) {
         self.init(dimensions: anchor.dimensions, category: anchor.category)
         components.set([AnchoringComponent(anchor)])
     }
-
+    
     public init(dimensions: simd_float3, category: CapturedRoom.Object.Category? = nil) {
         super.init()
-
+        
         let mesh = MeshResource.generateBox(size: .one, cornerRadius: .zero)
         let material = SimpleMaterial(color: .systemYellow, roughness: 0.27, isMetallic: false)
         let model = ModelComponent(mesh: mesh, materials: [material])
         let roomObject = RoomObjectComponent(dimensions: dimensions, category: category)
+        
         components.set([model, roomObject])
     }
-
-    fileprivate func update(_ anchor: RoomObjectAnchor) {
-        roomObject?.dimensions = anchor.dimensions
-        roomObject?.category = anchor.category
+    
+    public init(dimensions: simd_float3, category: CapturedRoom.Surface.Category) {
+        super.init()
+        let mesh = MeshResource.generateBox(size: .one, cornerRadius: .zero)
+        let material = SimpleMaterial(color: .systemYellow, roughness: 0.27, isMetallic: false)
+        let model = ModelComponent(mesh: mesh, materials: [material])
+        let roomSurface = RoomSurfaceComponent(dimensions: dimensions, category: category)
+        
+        components.set([model, roomSurface])
     }
-
+    
+    public init(dimensions: simd_float3, category: RoomElementCategory) {
+        super.init()
+        let mesh = MeshResource.generateBox(size: .one, cornerRadius: .zero)
+        let material = SimpleMaterial(color: .systemYellow, roughness: 0.27, isMetallic: false)
+        let model = ModelComponent(mesh: mesh, materials: [material])
+        
+        switch category {
+        case .object(let objectCategory):
+            let roomObject = RoomObjectComponent(dimensions: dimensions, category: objectCategory)  // Pass the extracted value
+            components.set([model, roomObject])
+        case .surface(let surfaceCategory):
+            let roomSurface = RoomSurfaceComponent(dimensions: dimensions, category: surfaceCategory)  // Pass the extracted value
+            components.set([model, roomSurface])
+        }
+    }
+    
+    fileprivate func update(_ anchor: RoomElementAnchor) {
+        switch anchor.category {
+        case .object(let objectCategory):
+            roomObject?.dimensions = anchor.dimensions
+            roomObject?.category = objectCategory
+        case .surface(let surfaceCategory):
+            roomSurface?.dimensions = anchor.dimensions
+            roomSurface?.category = surfaceCategory
+        }
+    }
+    
 }
 
 public extension Scene {
-
+    
     func addRoomObjectEntities(for anchors: [ARAnchor]) {
         addRoomObjectEntities(for: anchors.compactMap({ anchor in
-            anchor as? RoomObjectAnchor
+            anchor as? RoomElementAnchor
         }))
     }
-
+    
     func updateRoomObjectEntities(for anchors: [ARAnchor]) {
         updateRoomObjectEntities(for: anchors.compactMap({ anchor in
-            anchor as? RoomObjectAnchor
+            anchor as? RoomElementAnchor
         }))
     }
-
-    func addRoomObjectEntities(for roomObjectAnchors: [RoomObjectAnchor]) {
+    
+    func addRoomObjectEntities(for roomObjectAnchors: [RoomElementAnchor]) {
         for roomObjectAnchor in roomObjectAnchors {
             addAnchor(RoomObjectEntity(roomObjectAnchor))
         }
     }
-
-    func updateRoomObjectEntities(for roomObjectAnchors: [RoomObjectAnchor]) {
-        var roomObjectAnchorsByIdentifier = [UUID: RoomObjectAnchor]()
+    
+    func updateRoomObjectEntities(for roomObjectAnchors: [RoomElementAnchor]) {
+        var roomObjectAnchorsByIdentifier = [UUID: RoomElementAnchor]()
         for roomObjectAnchor in roomObjectAnchors {
             roomObjectAnchorsByIdentifier[roomObjectAnchor.identifier] = roomObjectAnchor
         }
-
+        
         for anchor in self.anchors {
             guard case .anchor(let identifier) = anchor.anchoring.target else { continue }
             guard let entity = anchor as? RoomObjectEntity else { continue }
@@ -98,17 +139,19 @@ public extension Scene {
             entity.update(roomObjectAnchor)
         }
     }
-
+    
 }
 
 public class RoomObjectSystem: System {
-
+    
     private let roomObjectAnchorQuery: EntityQuery
-
+    private let roomSurfaceAnchorQuery: EntityQuery
+    
     public required init(scene: Scene) {
         roomObjectAnchorQuery = EntityQuery(where: .has(RoomObjectComponent.self) && .has(ModelComponent.self))
+        roomSurfaceAnchorQuery = EntityQuery(where: .has(RoomSurfaceComponent.self) && .has(ModelComponent.self))
     }
-
+    
     public func update(context: SceneUpdateContext) {
         context.scene.performQuery(roomObjectAnchorQuery).forEach { entity in
             guard let entity = entity as? Entity & HasModel & HasRoomObjectComponent else { return }
@@ -116,14 +159,20 @@ public class RoomObjectSystem: System {
             entity.scale = roomObject.dimensions
             entity.model?.materials = [material(for: roomObject.category)]
         }
+        context.scene.performQuery(roomSurfaceAnchorQuery).forEach { entity in
+            guard let entity = entity as? Entity & HasModel & HasRoomSurfaceComponent else { return }
+            guard let roomSurface = entity.roomSurface else { return }
+            entity.scale = roomSurface.dimensions
+            entity.model?.materials = [material(for: roomSurface.category)]
+        }
     }
-
+    
     private func material(for category: CapturedRoom.Object.Category?) -> SimpleMaterial {
-        let roughness = MaterialScalarParameter(floatLiteral: 0.27)
+        let roughness = MaterialScalarParameter(floatLiteral: 1.0)
         guard let category = category else {
             return SimpleMaterial(color: .white, roughness: roughness, isMetallic: false)
         }
-
+        
         switch category {
         case .storage: return SimpleMaterial(color: .systemGreen, roughness: roughness, isMetallic: false)
         case .refrigerator: return SimpleMaterial(color: .systemBlue, roughness: roughness, isMetallic: false)
@@ -145,5 +194,23 @@ public class RoomObjectSystem: System {
             fatalError()
         }
     }
-
+    private func material(for category: CapturedRoom.Surface.Category?) -> SimpleMaterial {
+        let roughness = MaterialScalarParameter(floatLiteral: 1.0)
+        guard let category = category else {
+            return SimpleMaterial(color: .white, roughness: roughness, isMetallic: false)
+        }
+        
+        switch category {
+        case .wall: return SimpleMaterial(color: .systemGreen, roughness: roughness, isMetallic: false)
+        case .door(isOpen: true): return SimpleMaterial(color: .systemBlue, roughness: roughness, isMetallic: false)
+        case .door(isOpen: false): return SimpleMaterial(color: .systemYellow, roughness: roughness, isMetallic: false)
+        case .opening: return SimpleMaterial(color: .systemOrange, roughness: roughness, isMetallic: false)
+        case .window: return SimpleMaterial(color: .systemRed, roughness: roughness, isMetallic: false)
+        case .floor:
+            return SimpleMaterial(color: .systemCyan, roughness: roughness, isMetallic: false)
+        @unknown default:
+            fatalError()
+        }
+    }
+    
 }
